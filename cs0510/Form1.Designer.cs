8b735f0f9f5bbc082215ce9be0d8232c3bb475b9
nakayamashin0 <167387930+nakayamashin0@users.noreply.github.com>
2024-05-17 14:03:29 +0900

    Initial commit

---
+++ cs0510/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace cs0510
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Yu Gothic UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.Coral;
+            button1.Location = new Point(214, 67);
+            button1.Name = "button1";
+            button1.Size = new Size(215, 333);
+            button1.TabIndex = 0;
+            button1.Text = "botton1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Location = new Point(5, 10);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
